95be56288930859c977b6536a8910902112ee079
parth299 <sainiparth299@gmail.com>
2024-05-18 22:00:00 +0530

    Done with major components

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Footer from './footer/Footer';
 import Container from 'postcss/lib/container';
 import Logo from './Logo';
 import LogoutBtn from './header/LogoutBtn';
+import Select from './Select'
+import RTE from './RTE';
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Select
 };
